80527d25a14e9b3519c11bccea429c8b80888294
Tamara <tamara_tmb@hotmail.com>
2026-09-16 13:06:36 +0200

    feat: conclusion exercise 01

---
+++ Exercise01.php
@@ -0,0 +1,17 @@
+<?php
+
+$nombre = "Tamara";
+$edad = 34;
+$altura = 1.65;
+$estudiando = true;
+
+echo $nombre . PHP_EOL;
+echo $edad . PHP_EOL;
+echo $altura . PHP_EOL;
+echo $estudiando . PHP_EOL;
+
+define( "MI_NOMBRE", "Tamara" );
+
+echo "______" . PHP_EOL;
+echo MI_NOMBRE . PHP_EOL;
+echo "______" . PHP_EOL;
